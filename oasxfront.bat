5048798d889fd358d5b0f855f4d4586b91cf6ff4
Leo <1546614620@qq.com>
2026-07-06 08:51:16 +0800

    oasxfront

--- oasxfront.bat
+++ oasxfront.bat
@@ -1,9 +1,10 @@
 @echo off
-cd /d D:\onmyojitools\OASX\build\web
+cd /d %~dp0web
 
 echo ==========================
 echo  OASX Frontend Running
-echo  Close this window to stop
 echo ==========================
 
 python -m http.server 8080 --bind 0.0.0.0
+
+pause
